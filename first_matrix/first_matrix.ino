c933a71681a673c246e3ad359cfcc9f4db24b44a
pilli-pilli <pillitteri395@gmail.com>
2026-07-06 15:17:34 +0200

    basic turn functionality

--- first_matrix/first_matrix.ino
+++ first_matrix/first_matrix.ino
@@ -3,6 +3,10 @@
 #include <Adafruit_NeoPixel.h>
 #include <map>
 #include <vector>
+#include "battle.h"
+#include "game_state.h"
+#include "app_fsm.h"
+
 
 #define JOYSTICK_X 33
 #define JOYSTICK_Y 34
@@ -45,11 +49,12 @@ struct Boat {
 
 State current_state;
 std::vector<Boat> setup_boat;
+Role my_role;
 
 void setup() {
 
 
-  Serial.begin(9600);
+  Serial.begin(115200));
   pinMode(JOYSTICK_BUTTON, INPUT_PULLUP);
   pinMode(JOYSTICK_X, INPUT);
   pinMode(JOYSTICK_Y, INPUT);
@@ -60,7 +65,7 @@ void setup() {
 
   matrix.begin();
   matrix.setBrightness(10);
-
+  battle_begin();
   current_state = WAITING;
 }
 
@@ -95,7 +100,7 @@ void waiting() {
     delay(100);
   }
 
-  //registrati, aspetta server
+  my_role = battle_register();
   Serial.println("Button pressed!");
   current_state = SETUP;
 }
@@ -277,19 +282,29 @@ void setup_state() {
       
       continue;
     }
-
+    Serial.println("La inserisce?");
     setup_boat.push_back({ length, dir, { cursor_x, cursor_y } });
+    Serial.println("Yesssss");
     
   }
-  //MANDARE SEGNALE BARCHE POSIZIONATE
+
+  battle_send_setup(setup_boat);
+
+  matrix.clear();
+  matrix.show();
+  
   int timer = 0;
   //AGGIUNGERE CONTROLLO WAITING GAME START
-  while ( timer<5 ){
+  while ( timer<2 ){
+    Serial.println("Entra nel while colore");
 
     for (int i = MATRIX_HEIGHT-1; i >= 0; i--){
       for (int j = 0; j < MATRIX_WIDTH; j++){
         matrix.drawPixel(j, i, matrix.Color(0, 255, 0));
+        matrix.show();
+        delay(100);
         matrix.drawPixel(j, i, matrix.Color(0, 0, 0));
+        matrix.show();
       }
 
     }
@@ -299,9 +314,155 @@ void setup_state() {
   }
   while(digitalRead(JOYSTICK_BUTTON)){
     matrix.drawPixel(4, 4, matrix.Color(0, 255, 0));
+    matrix.show();
+  }
+
+  current_state = PLAYING;
+
+}
+
+void playing_state(){
+  
+  if( battle_my_turn() ){
+    matrix.clear();
+    for (int i = 0; i < MATRIX_HEIGHT; i++){
+      for(int j = 0; j < MATRIX_WIDTH; j++){
+        switch (g_state.enemy_board[j][i]){
+          case Unknown:
+            matrix.drawPixel(j, i, matrix.Color(0, 0, 0));
+            break;
+
+          case Hit:
+            matrix.drawPixel(j, i, matrix.Color(255, 128, 0));
+            break;
+
+          case Miss:
+            matrix.drawPixel(j, i, matrix.Color(0, 0, 255));
+            break;
+
+          case Sunk:
+            matrix.drawPixel(j, i, matrix.Color(255, 0, 0));
+            break;
+        }
+      }
+
+    }
+    
+
+    matrix.show();
+
+    while (digitalRead(JOYSTICK_BUTTON)) {
+      int x = readJoystickAxis(JOYSTICK_Y);
+      int y = readJoystickAxis(JOYSTICK_X);
+
+      int next_cursor_x = cursor_x + x;
+      int next_cursor_y = cursor_y + y;
+
+      if (next_cursor_x >= 0 && next_cursor_x < MATRIX_WIDTH && next_cursor_y >= 0 && next_cursor_y < MATRIX_HEIGHT ) {
+          
+        int previous_x = cursor_x;
+        int previous_y = cursor_y;
+        
+        matrix.drawPixel(cursor_x, cursor_y, matrix.Color(0,0,0));
+        cursor_x = next_cursor_x;
+        cursor_y = next_cursor_y;
+        matrix.drawPixel(cursor_x, cursor_y, matrix.Color(255,255,255));
+
+        switch(g_state.enemy_board[previous_x][previous_y]){
+          case Unknown:
+            matrix.drawPixel(previous_x, previous_y, matrix.Color(0, 0, 0));
+            break;
+
+          case Hit:
+            matrix.drawPixel(previous_x, previous_y, matrix.Color(255, 128, 0));
+            break;
+
+          case Miss:
+            matrix.drawPixel(previous_x, previous_y, matrix.Color(0, 0, 255));
+            break;
+
+          case Sunk:
+            matrix.drawPixel(previous_x, previous_y, matrix.Color(255, 0, 0));
+            break;
+        }
+      }
+    }
+
+    while (!digitalRead(JOYSTICK_BUTTON)) {
+      delay(100);
+    }
+
+    switch(battle_shoot(cursor_x, cursor_y)){
+      
+      case Hit:
+        matrix.drawPixel(cursor_x, cursor_y, matrix.Color(255, 128, 0));
+        break;
+
+      case Water:
+        matrix.drawPixel(cursor_x, cursor_y, matrix.Color(0, 0, 255));
+        break;
+
+      case Sunk:
+        matrix.drawPixel(cursor_x, cursor_y, matrix.Color(255, 0, 0));
+        break;
+    }
+
+    battle_await_change();
+
+
+  }else{
+    matrix.clear();
+    for (int i = 0; i < MATRIX_HEIGHT; i++){
+      for(int j = 0; j < MATRIX_WIDTH; j++){
+        switch (own_board[j][i]){
+          case Empty:
+            matrix.drawPixel(j, i, matrix.Color(0, 0, 0));
+            break;
+
+          case Hit:
+            matrix.drawPixel(j, i, matrix.Color(255, 128, 0));
+            break;
+
+          case Miss:
+            matrix.drawPixel(j, i, matrix.Color(0, 0, 255));
+            break;
+
+          case Sunk:
+            matrix.drawPixel(j, i, matrix.Color(255, 0, 0));
+            break;
+
+          case Ship:
+            matrix.drawPixel(j, i, matrix.Color(255, 255, 255));
+            break;
+        }
+      }
+
+    }
+
+
+    
+
+    matrix.show();
+    battle_await_change();
+
+  }
+
+  if(battle_over()){
+    current_state = RESULT;
+  }
+  
+}
+
+void result_state(){
+
+  if(my_role == battle_winner()){
+
+  }else{
+
   }
 
 
+  current_state = WAITING;
 }
 
 
@@ -315,6 +476,13 @@ void loop() {
     case SETUP:
       setup_state();
       break;
+
+    case PLAYING:
+      playing_state();
+      break;
+
+    case RESULT:
+      result_state();
   }
 
   delay(250);
